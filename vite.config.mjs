2e0e4544d38c74a3a98aaf9f96aa4eb9606e8055
muthukumar-29 <muthukumarm.2903@gmail.com>
2025-01-28 23:40:41 +0530

    update latest deploy

--- vite.config.mjs
+++ vite.config.mjs
@@ -38,6 +38,8 @@ export default defineConfig(() => {
           '.js': 'jsx',
         },
       },
+      include: ['react', 'react-dom'],
+      exclude: ['core-js'],
     },
     plugins: [react()],
     resolve: {
@@ -50,13 +52,16 @@ export default defineConfig(() => {
       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
     },
     server: {
-      port: 3000,
+      port: parseInt(process.env.PORT) || 3000,
       proxy: {
         // https://vitejs.dev/config/server-options.html
       },
       open: true,
       host: '0.0.0.0',
-      allowedHosts: ['treasure-hunt-admin.onrender.com']
+      allowedHosts: ['treasure-hunt-admin.onrender.com'],
+      hmr: {
+        overlay: false, // Disable error overlay
+      },
     },
   }
 })
